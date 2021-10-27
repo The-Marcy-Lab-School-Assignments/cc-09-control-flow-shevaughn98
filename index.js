1acd64549f5f790d97a3b23eb7aa920072f9ad5b
shevaughn98 <shevaughnlegall@gmail.com>
2021-10-27 15:07:55 +0000

    progress

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 //1
-function betweenFiveAndTen(){
+// function betweenFiveAndTen(num){
+//     if (num <  5 || 10 < num){
+//       return false
+//     }else{
+//         return true
+//     }
   
-}
-
+// }
+// console.log(betweenFiveAndTen(15))
 //2
-function betweenTenAndThirty(){
-  
-}
 
-//3
-function betweenThirtyAndFifty(){
+// function betweenTenAndThirty(num){
+//     if (num <= 10 || 30 < num){
+//         return false
+//     }else{
+//         return true
+//     }
   
-}
-
+// }
+// console.log(betweenTenAndThirty(15))
+// //3
+// function betweenThirtyAndFifty(num){
+//     if (num <= 30 || 50 > num){
+//         return true
+//     }else{
+//         return false 
+//     }
+    
+  
+// }
+// console.log(betweenThirtyAndFifty(55))
 
 //4
-function betweenThirtyFiftySeventy(){
+// function betweenThirtyFiftySeventy(num){
+//     if (num >= 0 && num <= 30 || num >= 50 && num <= 70 ){
+//         return true 
+//     }else{
+//         return false
+//     }
   
-}
-
+// }
+// console.log(betweenThirtyFiftySeventy(71))
 //5
-function betweenTwentyFiftySixty(){
+function betweenTwentyFiftySixty(num){
+    
   
 }
 
